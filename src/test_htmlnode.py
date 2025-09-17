2fdba92b38084ecd9e8d7257c17045217e659a68
Michał Kozłowski <10565786+mic-koz@users.noreply.github.com>
2025-09-17 18:00:56 +0200

    Add more unittest for HTMLNode class

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,11 @@
 import unittest
 from htmlnode import HTMLNode
 
-class TestTextNode(unittest.TestCase):
+class TestHTMLNode(unittest.TestCase):
     def test_repr_None(self):
         node = HTMLNode(None, None, None, None)
         self.assertEqual(
             "HTMLNode(None, None, None, None)", repr(node)        )
-
 
     def test_repr(self):
         node = HTMLNode("a", "foo", "bar", {"href": "https://www.google.com"})
@@ -30,6 +29,52 @@ class TestTextNode(unittest.TestCase):
         node = HTMLNode(None, None, None, props)
         attributes = node.props_to_html()
         self.assertEqual(" href=\"None\" target=\"_blank\"", attributes)
+
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
+
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
+
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, None, {'class': 'primary'})",
+        )
 
 if __name__ == "__main__":
     unittest.main()
