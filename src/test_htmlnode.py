5e6b73eb2f99e89d1b31bbc40fc4d9118ccde533
Michał Kozłowski <10565786+mic-koz@users.noreply.github.com>
2025-09-17 18:58:13 +0200

    Add class LeafNode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr_None(self):
@@ -75,6 +75,24 @@ class TestHTMLNode(unittest.TestCase):
             node.__repr__(),
             "HTMLNode(p, What a strange world, None, {'class': 'primary'})",
         )
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_no_value(self):
+        node = LeafNode("p", None)
+        self.assertRaises(ValueError)
+
+    def test_no_tag(self):
+        node = LeafNode(None, "foobar")
+        self.assertEqual(node.to_html(), "foobar")
+
+    def test_anchor(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
+
 
 if __name__ == "__main__":
     unittest.main()
